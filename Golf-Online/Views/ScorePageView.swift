afdff1b32fda2dce4bc72842b0572fc0cfadf7e2
Tristan Cortez <twcortez@gmail.com>
2022-12-09 16:11:36 -0800

    Restructured scorepage and scorecard via viewmodel

--- Golf-Online/Views/ScorePageView.swift
+++ Golf-Online/Views/ScorePageView.swift
@@ -7,25 +7,38 @@ struct ScorePageView: View {
     var viewModel = ViewModel()
     var body: some View {
         ScrollView {
-            VStack {
+            VStack(alignment: .leading, spacing: 0) {
+            }
+            .padding([.bottom], 10).frame(alignment: .leading)
                 HStack {
                     Text("\(Golf_Online.countryFlag(from: player.country))")
                         .padding([.trailing], 5)
                     Text("\(self.player.first_name) \(self.player.last_name)")
-                }.font(.title)
+                }.font(.title).padding([.bottom], 30)
 
                 ForEach(0 ..< totalRounds, id: \.self) { index in
-                    Text("Round: \(index + 1)")
+                    Text("Round \(index + 1)")
                         .font(.title3)
-                    ScorecardView(player: player)
-                }
-                .padding([.bottom], 10).frame(alignment: .leading)
+                    ScorecardView(viewModel: getScorecardViewModel(index: index))
+                Divider()
             }
             .onAppear(perform: {
                 scorecard = self.viewModel.getScorecard(player_id: player.player_id)
                 totalRounds = (scorecard?.results?.scorecard.count)!
-            })
+            }
+            ).padding([.leading], 20)
         }
+    }
+
+    private func getScorecardViewModel(index: Int) -> ScorecardView.ViewModel {
+        let courseData = scorecard?.results?.courses[0]
+        let holeDetails = scorecard?.results?.rounds_holes_breakdown.rounds[0].courses[0].holes
+        let holeScores = scorecard?.results?.scorecard[index].holes
+        let vm = ScorecardView.ViewModel(roundNumber: index,
+                                         courseData: courseData,
+                                         holeDetails: holeDetails,
+                                         holeScores: holeScores)
+        return vm
     }
 }
 
--- Golf-Online/Views/ScorecardView.swift
+++ Golf-Online/Views/ScorecardView.swift
@@ -1,16 +1,12 @@
 import SwiftUI
 
-struct ScorecardView: View {
-    @State private var scorecard: PGAScorecard?
-    @State private var courseData: CourseData?
-    @State private var holeDetails = [HoleDetail]()
-    @State private var holeScores = [HoleScore]()
-    @State private var backNineHoleDetails = [HoleDetail]()
-    @State private var backNineScores = [HoleScore]()
-    @State private var round = 0
+enum NineHoles {
+    case front
+    case back
+}
 
-    let player: LeaderboardPlayer
-    var viewModel = ViewModel()
+struct ScorecardView: View {
+    let viewModel: ViewModel
     // HOLE 1 2 3 4 5 6 7 8 9
     // -------------------------------
     // PAR
@@ -22,194 +18,187 @@ struct ScorecardView: View {
         VStack(spacing: 0) {
             // MARK: Front 9 Scorecard
 
-            Group {
-                HStack(spacing: 0) {
-                    Text("Hole").frame(width: 50, alignment: .leading)
-                    ForEach(holeDetails.prefix(9), id: \.hole_num) { detail in
-                        ZStack {
-                            Rectangle().opacity(0.2)
-                            Text(detail.hole_num)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
+            HStack(spacing: 0) {
+                Text("Hole").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHolesDetails(whichNine: .front), id: \.hole_num) { detail in
                     ZStack {
                         Rectangle().opacity(0.2)
-                        Text("OUT")
-                            .fixedSize(horizontal: false, vertical: false)
+                        Text(detail.hole_num)
                     }
-                    .frame(width: 40, height: 20)
+                    .frame(width: 25, height: 20)
+                }
+                ZStack {
                     Rectangle().opacity(0.2)
-                        .frame(width: 40, height: 20)
+                    Text("OUT")
+                        .fixedSize(horizontal: false, vertical: false)
                 }
-                HStack(spacing: 0) {
-                    Text("Par").frame(width: 50, alignment: .leading)
-                    ForEach(holeDetails.prefix(9), id: \.hole_num) { detail in
-                        ZStack {
-                            Rectangle().opacity(0.1)
-                            Text(detail.par)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
+                .frame(width: 40, height: 20)
+                Rectangle().opacity(0.2)
+                    .frame(width: 40, height: 20)
+            }
+            HStack(spacing: 0) {
+                Text("Par").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHolesDetails(whichNine: .front), id: \.hole_num) { detail in
                     ZStack {
                         Rectangle().opacity(0.1)
-                        Text("\(getFrontNinePar())")
+                        Text(detail.par)
                     }
-                    .frame(width: 40, height: 20)
-                    Rectangle().opacity(0.1)
-                        .frame(width: 40, height: 20)
+                    .frame(width: 25, height: 20)
                 }
-                HStack(spacing: 0) {
-                    Text("Score").frame(width: 50, alignment: .leading)
-                    ForEach(holeScores.prefix(9), id: \.hole_number) { score in
-                        ZStack {
-                            Rectangle().opacity(0.1)
-                            Text(score.score)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getNinePar(whichNine: .front))")
+                }
+                .frame(width: 40, height: 20)
+                Rectangle().opacity(0.1)
+                    .frame(width: 40, height: 20)
+            }
+            HStack(spacing: 0) {
+                Text("Score").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHoleScores(whichNine: .front), id: \.hole_number) { score in
                     ZStack {
                         Rectangle().opacity(0.1)
-                        Text("\(getFrontNineScore())")
+                        Text(score.score)
                     }
-                    .frame(width: 40, height: 20)
-                    Rectangle().opacity(0.1)
-                        .frame(width: 40, height: 20)
+                    .frame(width: 25, height: 20)
                 }
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getTotalNineScores(whichNine: .front))")
+                }
+                .frame(width: 40, height: 20)
+                Rectangle().opacity(0.1)
+                    .frame(width: 40, height: 20)
             }
 
             // MARK: Back 9 Scorecard
 
-            Group {
-                HStack(spacing: 0) {
-                    Text("Hole").frame(width: 50, alignment: .leading)
-                    ForEach(backNineHoleDetails, id: \.hole_num) { detail in
-                        ZStack {
-                            Rectangle().opacity(0.2)
-                            Text(detail.hole_num)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
+            Divider().frame(width: 355, height: 3).overlay(.black)
+
+            HStack(spacing: 0) {
+                Text("Hole").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHolesDetails(whichNine: .back), id: \.hole_num) { detail in
                     ZStack {
                         Rectangle().opacity(0.2)
-                        Text("IN")
+                        Text(detail.hole_num)
                     }
-                    .frame(width: 40, height: 20)
-                    ZStack {
-                        Rectangle().opacity(0.2)
-                        Text("TOT")
-                    }
-                    .frame(width: 40, height: 20)
+                    .frame(width: 25, height: 20)
                 }
-                HStack(spacing: 0) {
-                    Text("Par").frame(width: 50, alignment: .leading)
-                    ForEach(backNineHoleDetails, id: \.hole_num) { detail in
-                        ZStack {
-                            Rectangle().opacity(0.1)
-                            Text(detail.par)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
-                    ZStack {
-                        Rectangle().opacity(0.1)
-                        Text("\(getBackNinePar())")
-                    }
-                    .frame(width: 40, height: 20)
-                    ZStack {
-                        Rectangle().opacity(0.1)
-                        Text("\(getParTotal())")
-                    }
-                    .frame(width: 40, height: 20)
+                ZStack {
+                    Rectangle().opacity(0.2)
+                    Text("IN")
                 }
-                HStack(spacing: 0) {
-                    Text("Score").frame(width: 50, alignment: .leading)
-                    ForEach(backNineScores, id: \.hole_number) { score in
-                        ZStack {
-                            Rectangle().opacity(0.1)
-                            Text(score.score)
-                        }
-                        .frame(width: 25, height: 20)
-                    }
-                    ZStack {
-                        Rectangle().opacity(0.1)
-                        Text("\(getBackNineScore())")
-                    }
-                    .frame(width: 40, height: 20)
-                    ZStack {
-                        Rectangle().opacity(0.1)
-                        Text("\(getTotalScore())")
-                    }
-                    .frame(width: 40, height: 20)
+                .frame(width: 40, height: 20)
+                ZStack {
+                    Rectangle().opacity(0.2)
+                    Text("TOT")
                 }
+                .frame(width: 40, height: 20)
+            }
+            HStack(spacing: 0) {
+                Text("Par").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHolesDetails(whichNine: .back), id: \.hole_num) { detail in
+                    ZStack {
+                        Rectangle().opacity(0.1)
+                        Text(detail.par)
+                    }
+                    .frame(width: 25, height: 20)
+                }
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getNinePar(whichNine: .back))")
+                }
+                .frame(width: 40, height: 20)
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getParTotal())")
+                }
+                .frame(width: 40, height: 20)
+            }
+            HStack(spacing: 0) {
+                Text("Score").frame(width: 50, alignment: .leading)
+                ForEach(viewModel.getNineHoleScores(whichNine: .back), id: \.hole_number) { score in
+                    ZStack {
+                        Rectangle().opacity(0.1)
+                        Text(score.score)
+                    }
+                    .frame(width: 25, height: 20)
+                }
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getTotalNineScores(whichNine: .back))")
+                }
+                .frame(width: 40, height: 20)
+                ZStack {
+                    Rectangle().opacity(0.1)
+                    Text("\(viewModel.getTotalScore())")
+                }
+                .frame(width: 40, height: 20)
             }
         }
-        .onAppear(perform: {
-            scorecard = self.viewModel.getScorecard(player_id: player.player_id)
-            round = (scorecard?.results?.rounds_holes_breakdown.rounds.count)!
-            setRoundScore(round: round - 1)
-        })
-    }
-
-    func getTotalScore() -> Int {
-        return getFrontNineScore() + getBackNineScore()
-    }
-
-    func getFrontNineScore() -> Int {
-        var frontScore = 0
-        for score in holeScores.prefix(9) {
-            frontScore += Int(score.score)!
-        }
-        return frontScore
-    }
-
-    func getBackNineScore() -> Int {
-        var backScore = 0
-        for score in backNineScores {
-            backScore += Int(score.score)!
-        }
-        return backScore
-    }
-
-    func getFrontNinePar() -> Int {
-        if let frontNinePar = courseData?.par_front {
-            return frontNinePar
-        }
-        return 0
-    }
-
-    func getBackNinePar() -> Int {
-        if let backNinePar = courseData?.par_back {
-            return backNinePar
-        }
-        return 0
-    }
-
-    func getParTotal() -> Int {
-        if let parTotal = courseData?.par_total {
-            return parTotal
-        }
-        return 0
-    }
-
-    func setRoundScore(round: Int) {
-        courseData = (scorecard?.results?.courses[0])!
-        holeScores = (scorecard?.results?.scorecard[round].holes)!
-        backNineScores = Array((scorecard?.results?.scorecard[round].holes.suffix(from: 9))!)
-
-        holeDetails = (scorecard?.results?.rounds_holes_breakdown.rounds[0].courses[0].holes)!
-        backNineHoleDetails = Array((scorecard?.results?.rounds_holes_breakdown.rounds[0].courses[0].holes.suffix(from: 9))!)
     }
 }
 
 extension ScorecardView {
     struct ViewModel {
-        func getScorecard(player_id: Int) -> PGAScorecard {
-            return GolfDataService().getScorecard(player_id: player_id)
+        var roundNumber: Int
+        var courseData: CourseData?
+        var holeDetails: [HoleDetail]?
+        var holeScores: [HoleScore]?
+
+        func getTotalScore() -> Int {
+            let frontScore = getTotalNineScores(whichNine: .front)
+            let backScore = getTotalNineScores(whichNine: .back)
+            return (frontScore + backScore)
+        }
+
+        func getNineHolesDetails(whichNine: NineHoles) -> [HoleDetail] {
+            if let holeDetails = holeDetails {
+                switch whichNine {
+                case .front:
+                    return Array(holeDetails.prefix(9))
+                case .back:
+                    return Array(holeDetails.suffix(9))
+                }
+            }
+
+            return [HoleDetail]()
+        }
+
+        func getNineHoleScores(whichNine: NineHoles) -> [HoleScore] {
+            if let holeScores = holeScores {
+                switch whichNine {
+                case .front:
+                    return Array(holeScores.prefix(9))
+                case .back:
+                    return Array(holeScores.suffix(9))
+                }
+            }
+            return [HoleScore]()
+        }
+
+        func getTotalNineScores(whichNine: NineHoles) -> Int {
+            var totalScore = 0
+            let nineScores = getNineHoleScores(whichNine: whichNine)
+            for score in nineScores {
+                totalScore += Int(score.score)!
+            }
+            return totalScore
+        }
+
+        func getNinePar(whichNine: NineHoles) -> Int {
+            switch whichNine {
+            case .front:
+                return courseData?.par_front ?? 0
+            case .back:
+                return courseData?.par_back ?? 0
+            }
+        }
+
+        func getParTotal() -> Int {
+            let frontPar = courseData?.par_front ?? 0
+            let backPar = courseData?.par_back ?? 0
+            return (frontPar + backPar)
         }
     }
-}
-
-extension UIScreen {
-    static let screenWidth = UIScreen.main.bounds.size.width
-    static let screenHeight = UIScreen.main.bounds.size.height
-    static let screenSize = UIScreen.main.bounds.size
 }
